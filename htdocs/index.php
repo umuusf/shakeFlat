a582f9e789e41e03664dbd89acfda6e1af78459d
umuu <umuu.sf@gmail.com>
2022-06-04 11:02:20 +0900

    remove test code, rename template fnc

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,5 +8,5 @@
 require_once __DIR__ . "/../shakeFlat/core/autoloader.inc";
 
 $app = new shakeFlat\App();
-$app->setTransaction()->setTemplateLayout("default")->setMode(shakeFlat\Template::MODE_WEB);
+$app->setTransaction()->setTemplate("default")->setMode(shakeFlat\Template::MODE_WEB);
 $app->execModule()->publish();
