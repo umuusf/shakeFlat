ecb40b67b530a30cdbcde77dab7f0f1151404afd
umuu <umuu.sf@gmail.com>
2022-07-11 23:26:46 +0900

    support multi language

--- htdocs/index.php
+++ htdocs/index.php
@@ -10,5 +10,6 @@
 require_once __DIR__ . "/../shakeFlat/core/autoloader.inc";
 
 $app = new shakeFlat\App();
+$app->setTranslationLang("kr");
 $app->setTemplate("default")->setMode(shakeFlat\Template::MODE_WEB);
 $app->execModule()->publish();
--- shakeFlat/modules/welcome/sample.php
+++ shakeFlat/modules/welcome/sample.php
@@ -17,25 +17,39 @@ use shakeFlat\libs\Token;
 use shakeFlat\libs\AuthSession;
 use shakeFlat\models\mUser;
 
-function fnc_sample()
+function fnc_sample($app)
 {
     // use web template
     sfModeWEB();
 
+    $app->setTranslationLang("kr");
+
     // check paramaters.
     $param = Param::getInstance();
-    $param->checkKeyValue("age", Param::TYPE_INT);
+    $param->check("user_no", Param::TYPE_INT);
+    $param->check("age", Param::TYPE_INT);
     $param->check("name", Param::TYPE_STRING);
 
-    // To do...
+
+    // DB Query test
+    /*
     $db = DB::getInstance();
-    $rs = $db->query("select * from user where age = :age", array( ":age" => $param->age ));
+    $rs = $db->query("select * from user where age = :age", array( ":age" => $param->_d_age ));
     $row = $db->fetch($rs);
+    */
+
+    // OORO
+    /*
+    $mUser = mUser::getInstance($param->user_no);
+    if ($mUser->name == $param->name) {
+        $mUser->age = $param->_d_age;
+    }
+    */
 
     // response
     $res = Response::getInstance();
-    $res->age = $param->age;
-    $res->name = $param->_d_name;
-    $res->msg = "This is a demo module.";
-    $res->row = $row;
+    $res->age = $param->_d_age;
+    $res->name = $param->name;
+    $res->msg = "[0:This is a demo module.:]";
+    //$res->row = $row;
 }
