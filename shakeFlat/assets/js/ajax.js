5a6b75ddf8d6a88c20bcc3f260f6deaa73b4e1f6
umuu <umuu.sf@gmail.com>
2023-02-19 15:08:40 +0900

    admin update

--- shakeFlat/assets/js/ajax.js
+++ shakeFlat/assets/js/ajax.js
@@ -1,43 +1,46 @@
 function callAjax(fnc, postdata, successCallback, errorCallback, _this)
 {
     $.ajax({
-        url: "/" + fnc,
+        url: fnc,
         method: "POST",
         data: postdata,
         xhrFields: { withCredentials: true },
         statusCode: {
             404: function() {
-                alert('Page is not found. (404)');
+                alert('페이지를 찾을 수 없습니다. (404)');
                 return false;
             },
             500: function() {
-                alert('Internal server error. (500)');
+                alert('서버가 응답이 없습니다. (500)');
                 return false;
             }
         }
     }).done(function(result, textStatus, jqXHR) {
-        switch(result.error.errCode) {
-            case 0 :
-                return successCallback(result.data, _this);
-            case -9999 :
-                alertJump('Please login.', '/auth/login');
-                return false;
-            default :
-                console.log(result);
-                //console.log(textStatus);
-                //console.log(jqXHR);
-                if (errorCallback) {
-                    errorCallback(result, _this);
-                } else {
-                    msg = result.error.errMsg;
-                    if (!msg) msg = "The wrong approach. Please try again in a few minutes.";
-                    alert(msg + " (" + result.error.errCode + ")");
-                }
-                return false;
+        if (!result || result.constructor != Object || !("error" in result) || !("errCode" in result.error)) {
+            console.log(result);
+            //console.log(textStatus);
+            //console.log(jqXHR);
+        } else {
+            switch(result.error.errCode) {
+                case 0 :
+                    return successCallback(result, _this);
+                case -9999 :
+                    alertJump('로그인이 필요합니다.', '/auth/login');
+                    return false;
+                default :
+                    if (errorCallback) {
+                        errorCallback(result, _this);
+                    } else {
+                        msg = result.error.errMsg;
+                        if (!msg) msg = "잘못된 접근입니다. 잠시 후 다시 시도해주세요.";
+                        alert(msg + " (" + result.error.errCode + ")");
+                    }
+                    return false;
+            }
         }
     }).fail(function(e) {
         console.log(e);
-        alert("A problem occurred when calling the server. Please try again in a few minutes.");
+        alert("서버 호출시 문제가 발생하였습니다. 잠시 후 다시 시도해주세요.");
         return false;
     });
 }
